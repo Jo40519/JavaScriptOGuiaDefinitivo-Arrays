26cf87a9aed8bf7af56744f16dadbf84f824541c
João Vitor Nunes Lobato de Souza <jaolobato85@gmail.com>
2022-05-19 16:06:54 -0300

    Métodos indexOf() e lastIndexOf()

--- filter().js
+++ filter().js
@@ -1,4 +1,4 @@
-let a = [5, 4, 3, 2, 1]
+let a = [5, 4, 3, 2, 1, 2, 2, 2,2, 2, 5, 5, 5, 5]
 
 let smallvalues = a.filter(e => {
     return e < 3
@@ -10,3 +10,8 @@ let everyother = a.filter((e, i) => {
 
 console.log(smallvalues)
 console.log(everyother)
+
+
+let b = [...new Set(a)]
+
+console.log(b)
---
+++ indexOf()-e-lastIndexOf().js
@@ -0,0 +1,8 @@
+let a = [0, 1, 2, 1, 0];
+
+let b = a.indexOf(1)
+console.log(b)
+let c = a.lastIndexOf(1)
+console.log(c)
+let d = a.indexOf(3)
+console.log(d)
