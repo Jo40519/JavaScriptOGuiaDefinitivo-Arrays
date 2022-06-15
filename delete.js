1e01da18253930699f4bcd99e20fc544f81a7e89
João Vitor Nunes Lobato de Souza <jaolobato85@gmail.com>
2022-06-15 09:17:08 -0300

    Update delete.js

--- delete.js
+++ delete.js
@@ -1,16 +1,36 @@
 // a = [1, 2, 3]
 
-// delete a[0] 
+// delete a[0]
 // console.log(a)
 
 
 
-let l = [1, 2, 'a', 'b']
+// let l = [1, 2, 'a', 'b']
 
-let newList = l.filter(element => {
-    if (typeof element === 'number') {
-        return element
-    }
-})
+// let newList = l.filter(element => {
+//     if (typeof element === 'number') {
+//         return element
+//     }
+// })
 
-console.log(newList)
+// console.log(newList)
+
+
+// function friend(friends = []) {
+//     let arr = friends.filter((element, i) => {
+//         if (element.length == 4) {
+//             return element
+//         }
+//     })
+//     return arr
+// }
+
+// console.log(friend(["Ryan", "Kieran", "Mark"]))
+
+let numbers = 12
+let arr = [1, 5].sort((a, b) => {
+    return b - a
+}).join('')
+let num = Number(arr)
+console.log(num)
+console.log(arr)
